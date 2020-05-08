5fee574129b0530efed65e6ed3d87a13c1d7a5be
Hyago Brito <hyago-brito@hotmail.com>
2020-05-08 09:20:16 -0300

    Application to display Github user info.

---
+++ modulo06/src/routes.js
@@ -0,0 +1,26 @@
+import {createAppContainer} from 'react-navigation';
+import {createStackNavigator} from 'react-navigation-stack';
+
+import Main from './pages/Main/index';
+import User from './pages/User/index';
+
+const Routes = createAppContainer(
+  createStackNavigator(
+    {
+      Main,
+      User,
+    },
+    {
+      defaultNavigationOptions: {
+        headerTitleAlign: 'center',
+        headerBackTitleVisible: false,
+        headerStyle: {
+          backgroundColor: '#7159c1',
+        },
+        headerTintColor: '#FFF',
+      },
+    }
+  )
+);
+
+export default Routes;
